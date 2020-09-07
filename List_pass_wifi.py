ddcdb146236bd8ba3fbb71bcbf101daaef44b56e
Youssefkhalil4444 <70852600+Youssefkhalil4444@users.noreply.github.com>
2020-09-07 06:55:50 +0200

    Create List_pass_wifi.py

---
+++ List_pass_wifi.py
@@ -0,0 +1,29 @@
+import os
+
+os. system ("clear")
+
+def login (name) :
+
+    name = input ("Type Password please: ")
+
+    if name == ("mix DAROID") :
+
+        print ("welcome.........!")
+
+    else :
+
+        print ("Error"*100000000)
+
+print ("& & & & & & & & & & & & & & & & &"*100)
+
+print ("Iam youssef welcome in my script....!")
+
+login ("gjfjfhfj")
+
+start = 10000000
+
+while start < 999999999 :
+
+    start=start+1
+
+    print (start)
